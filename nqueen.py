e25f81403a199e87e591b16f7be9a60a872e4316
Cristiano Caon <cristiano.e.caon@gmail.com>
2022-10-19 20:07:10 -0500

    refactored code

--- nqueen.py
+++ nqueen.py
@@ -1,14 +1,13 @@
 import sys
 
 
-def main():
-    """Generates the CNF file for the N-Queens SAT problem.
+def get_row_clauses(num):
+    """Gets row clauses for N-Queens SAT problem formula.
+
+    :param int num: board dimension.
+    :return list: formula with row clauses.
     """
-    
     formula = []
-    num = int(sys.argv[1]) if len(sys.argv) > 1 else 3
-    
-    # Row restriction
     for i in range(num):
         chosen = 1
         for _ in range(num):
@@ -21,8 +20,17 @@ def main():
             chosen += 1
             clause.append(0)
             formula.append(clause)
-        
-    # Column restriction
+            
+    return formula
+
+
+def get_column_clauses(num):
+    """Gets column clauses for N-Queens SAT problem formula.
+
+    :param int num: board dimension.
+    :return list: formula with column clauses.
+    """
+    formula = []
     for i in range(1, num + 1):
         chosen = i
         for _ in range(num):
@@ -35,16 +43,33 @@ def main():
             chosen += num
             clause.append(0)
             formula.append(clause)
-        
-    # Board generation for diagonal restriction
+            
+    return formula
+
+
+def generate_board(num):
+    """Generates N-Queens board.
+
+    :param int num: board dimension.
+    :return list: N-Queens board
+    """
     board = []    
     for i in range(num):
         row = []
         for j in range(num):
             row.append((j + 1) + i * num)
         board.append(row)
-            
-    # Diagonal restriction 
+        
+    return board
+
+def get_diagonal_clauses(num, board):
+    """Gets diagonal clauses for N-Queens SAT problem formula.
+
+    :param int num: board dimension.
+    :param list board: N-Queens board.
+    :return list: formula with diagonal clauses.
+    """
+    formula = []
     valid_y = range(num)
     for i in range(num):
         for j in range(num):
@@ -62,13 +87,36 @@ def main():
                         diag2[index] = number * -1
                 diag2.append(0)
                 formula.append(diag2)
+                
+    return formula
 
-    # Save formula into CNF
-    with open("nqueen.cnf", 'w') as file:
-        file.write(f"c nqueen.cnf\nc\np cnf {num * num} {len(formula)}\n")
+
+def write_cnf_formula(formula, file, num):
+    """Writes N-Queens formula clauses to CNF file.
+
+    :param list formula: formula with N-Queens clauses.
+    :param str file: filename to be written.
+    :param int num: board dimension.
+    """
+    with open(file, 'w') as f:
+        f.write(f"c {file}\nc\np cnf {num * num} {len(formula)}\n")
         for clause in formula:
             temp = " ".join(map(str, clause))
-            file.write(f"{temp}\n")
+            f.write(f"{temp}\n")
+
+def main():
+    """Generates the CNF file for the N-Queens SAT problem.
+    """
+    num = int(sys.argv[1]) if len(sys.argv) > 1 else 3
+    
+    row_formula = get_row_clauses(num)
+    column_formula = get_column_clauses(num)
+    board = generate_board(num)
+    diagonal_formula = get_diagonal_clauses(num, board)
+
+    formula = [*row_formula, *column_formula, *diagonal_formula]
+    
+    write_cnf_formula(formula, "nqueen.cnf", num)
         
 
 if __name__ == '__main__':
